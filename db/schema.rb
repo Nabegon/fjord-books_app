7b7f4bfcd8e6beeabc0a3839c5a320589fb98301
Miki Watanabe <watami03@gmail.com>
2021-08-22 12:08:22 +0200

    fixed the points noted in the first review.

--- db/schema.rb
+++ db/schema.rb
@@ -50,8 +50,8 @@ ActiveRecord::Schema.define(version: 2021_07_13_144011) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["followed_id"], name: "index_relationships_on_followed_id"
