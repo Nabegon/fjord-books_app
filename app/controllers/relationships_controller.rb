1bef58fe9da81a7a7f2ab5a61d826110986ac084
Miki Watanabe <watami03@gmail.com>
2021-08-25 20:37:07 +0200

    add the flash messages for following/unfollowing users

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,22 +4,14 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
+    flash[:notice] = 'フォローに成功しました'
     redirect_to user
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
+    flash[:notice] = 'アンフォローしました'
     redirect_to user
   end
-
-  # def followings
-  #   user = User.find(params[:user_id])
-  #   @users = user.followings
-  # end
-
-  # def followers
-  #   user = User.find(params[:user_id])
-  #   @users = user.followers
-  # end
 end
