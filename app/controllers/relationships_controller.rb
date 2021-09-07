561a98f61d0a32b03e3b31cdb44b5317ad275df4
Miki Watanabe <watami03@gmail.com>
2021-09-07 11:06:14 +0200

    refactor: flash message and redirect into one line

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,14 +4,12 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    flash[:notice] = 'フォローに成功しました'
-    redirect_to user
+    redirect_to user, notice: 'フォローに成功しました'
   end
 
   def destroy
     user = current_user.active_relationships.find(params[:id]).followed
     current_user.unfollow(user)
-    flash[:notice] = 'アンフォローしました'
-    redirect_to user
+    redirect_to user, notice: 'アンフォローしました'
   end
 end
