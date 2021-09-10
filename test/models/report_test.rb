96d1ce7ed5fde88504e6395494cbe538b410dc7c
Miki Watanabe <watami03@gmail.com>
2021-09-10 12:13:14 +0200

    test: system tests for book and report, a model test for report.

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,18 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test "editable" do
+  test 'editable' do
     target_user = User.create!(email: 'target@example.com', password: '666666')
-    no_target_user = User.create!(email: 'iamnottarget@example.com', password: '555555')    
+    no_target_user = User.create!(email: 'iamnottarget@example.com', password: '555555')
 
     post = Report.new(user_id: target_user.id, title: 'test', content: 'test')
     assert post.editable?(target_user)
     assert_not post.editable?(no_target_user)
   end
 
-  test "created_on" do
+  test 'created_on' do
     user = User.create!(email: 'target@example.com', password: '666666')
     post = Report.create!(user_id: user.id, title: 'test', content: 'test')
-    assert post.created_on == (Date.current)     
+    assert post.created_on == (Date.current)
   end
 end
