5742eb3e87d789a4ed722d1bf74b0c92864c8903
Miki Watanabe <watami03@gmail.com>
2021-10-08 20:05:29 +0200

    migrate changes from broken branch

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @comment = @commentable.comments.new comment_params
+    @comment.user_id = current_user.id
+    if @comment.save!
+      redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+    else
+      flash.now[:alert] = 'コメントに失敗しました'
+      redirect_to polymorphic_url([@commentable])
+    end
+  end
+
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
